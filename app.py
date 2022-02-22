19d14e8a4d368ce4e63dd3353d11fce913c80eac
Vignesh-Seetharaman <78829747+Vignesh-Seetharaman@users.noreply.github.com>
2022-02-22 17:32:21 +0530

    Added Model.py

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import numpy as np
-import pickle
+import model
 import warnings
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
-
-with open('pickle/User_Recommendation.pkl', 'rb') as data:
-    userRecommendation = pickle.load(data)
-
-with open('pickle/Logistic_Regression.pkl', 'rb') as model:
-    best_model = pickle.load(model)
-
-with open('pickle/Vectorizer.pkl', 'rb') as vectorizer:
-    tfidf = pickle.load(vectorizer)
-
-def get_sentiment(text):
-    """
-    Predicts the sentiment of text using the Multinomial Naive Bayes Model
-    """
-    sentiment_id = best_model.predict(tfidf.transform([text]))
-    return sentiment_id
 
 @app.route("/")
 def home():
@@ -30,17 +14,10 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     username = request.form.get('username')
-    print(username)
-    d = userRecommendation.loc[str(username)].sort_values(ascending=False)[0:20]
-    ratings = pd.read_csv('data/ratings_df.csv' , encoding='latin-1')
-    output_user = pd.merge(d,ratings,left_on='name',right_on='name',how='left')
-    rows = []
-    for text,name in zip(output_user['reviews_text'],output_user['name']):
-        rows.append([name,text,get_sentiment(text)])
-    
-    df = pd.DataFrame(rows, columns=["product","review", "sentiment"])
-
-    return render_template("index.html", OUTPUT=((df.groupby('product')['sentiment'].sum() / df.groupby('product')['review'].count())*100).sort_values(ascending=False)[0:5])
+    finalRecommendation = model.predict_top5(username)
+    output_df = pd.DataFrame(finalRecommendation, columns=["product","percentage"])
+    output = [output_df.to_html(classes='output_df')]
+    return render_template("index.html", OUTPUT= output)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+with open('pickle/recommendation.pkl', 'rb') as data:
+    userRecommendation = pickle.load(data)
+
+with open('pickle/model.pkl', 'rb') as model:
+    best_model = pickle.load(model)
+
+with open('pickle/tfidf_vectorizer.pkl', 'rb') as vectorizer:
+    tfidf = pickle.load(vectorizer)
+
+def get_sentiment(text):
+    sentiment_id = best_model.predict(tfidf.transform([text]))
+    return sentiment_id
+
+def predict_top5(username):
+    print(username)
+    d = userRecommendation.loc[str(username)].sort_values(ascending=False)[0:20]
+    ratings = pd.read_csv('data/ratings_df.csv' , encoding='latin-1')
+    output_user = pd.merge(d,ratings,left_on='name',right_on='name',how='left')
+    rows = []
+    for text,name in zip(output_user['reviews_text'],output_user['name']):
+        rows.append([name,text,get_sentiment(text)])
+    
+    df = pd.DataFrame(rows, columns=["product","review", "sentiment"])
+    output = pd.DataFrame(((df.groupby('product')['sentiment'].sum() / df.groupby('product')['review'].count())*100).sort_values(ascending=False)[0:5])
+    output = output.reset_index()
+    output_list = output.values.tolist()
+
+    return output_list
