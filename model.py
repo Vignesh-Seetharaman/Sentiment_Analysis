4a9952f033a760784009c87f23efe46b4defa1df
Vignesh-Seetharaman <78829747+Vignesh-Seetharaman@users.noreply.github.com>
2022-02-23 10:39:39 +0530

    Modified Model.py

--- model.py
+++ model.py
@@ -11,17 +11,15 @@ with open('pickle/model.pkl', 'rb') as model:
 with open('pickle/tfidf_vectorizer.pkl', 'rb') as vectorizer:
     tfidf = pickle.load(vectorizer)
 
-def get_sentiment(text):
-    sentiment_id = best_model.predict(tfidf.transform([text]))
-    return sentiment_id
-
 def predict_top5(username):
     top_20 = userRecommendation.loc[str(username)].sort_values(ascending=False)[0:20]
     ratings = pd.read_csv('data/ratings_df.csv' , encoding='latin-1')
     output_user = pd.merge(top_20,ratings,left_on='name',right_on='name',how='left')
+    tfidf_list = tfidf.transform(output_user['reviews_text'])
+    sentiment_list = best_model.predict(tfidf_list)
     rows = []
-    for text,name in zip(output_user['reviews_text'],output_user['name']):
-        rows.append([name,text,get_sentiment(text)])
+    for text,name,sentiment in zip(output_user['reviews_text'],output_user['name'],sentiment_list):
+        rows.append([name,text,sentiment])
     
     df = pd.DataFrame(rows, columns=["product","review", "sentiment"])
     output = pd.DataFrame(((df.groupby('product')['sentiment'].sum() / df.groupby('product')['review'].count())*100).sort_values(ascending=False)[0:5])
